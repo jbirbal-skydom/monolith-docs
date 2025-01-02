7e54cfcc3e3f07b6d3c2e990d939ddc51f0d36f0
Jason <skydom@zoho.com>
2025-01-02 21:30:38 +0300

    tutorial > doc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
 
         {
